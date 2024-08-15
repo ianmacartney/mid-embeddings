dd6484d69c9285a3999bf9d7c10f0317c5381a9f
Ian Macartney <ianhmacartney@gmail.com>
2024-08-15 01:51:50 -0700

    rename, don't clip overflow

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -12,7 +12,7 @@ export const Route = createRootRoute({
         <nav className="container w-full justify-between flex flex-row items-center gap-6">
           <div className="flex items-center gap-6 md:gap-10">
             <a href="/">
-              <h1 className="text-base font-semibold">Feelings Wheel</h1>
+              <h1 className="text-base font-semibold">Mid</h1>
             </a>
             <div className="flex items-center gap-4 text-sm">
               <a
@@ -37,11 +37,11 @@ export const Route = createRootRoute({
           </Authenticated>
         </nav>
       </header>
-      <main className="flex grow flex-col overflow-hidden">
+      <main className="flex grow flex-col">
         <Outlet />
         <Toaster />
       </main>
-      <footer className="border-t hidden sm:block">
+      <footer className="border-t hidden grow-0 sm:block">
         <div className="flex">
           <div className="container py-4 text-sm leading-loose">
             Built with ❤️ at{" "}
