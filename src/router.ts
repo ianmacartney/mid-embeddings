a9eccea5b208675df5a20e0145be879f1c530585
Ian Macartney <ianhmacartney@gmail.com>
2024-08-19 21:33:43 -0700

    serve on mid-embeddings on labs

--- src/router.ts
+++ src/router.ts
@@ -3,7 +3,10 @@ import { createRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
 
 // Create a new router instance
-export const router = createRouter({ routeTree });
+export const router = createRouter({
+  routeTree,
+  basepath: import.meta.env.VITE_BASEPATH,
+});
 
 // Register the router instance for type safety
 declare module "@tanstack/react-router" {
