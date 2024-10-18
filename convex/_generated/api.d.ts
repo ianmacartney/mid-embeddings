f63a5f1a106111d571540cd5866755905090c526
Ian Macartney <ianhmacartney@gmail.com>
2024-10-17 18:07:41 -0700

    add components to convex.config.ts

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -10,11 +10,6 @@
  * @module
  */
 
-import type {
-  ApiFromModules,
-  FilterApi,
-  FunctionReference,
-} from "convex/server";
 import type * as auth from "../auth.js";
 import type * as embed from "../embed.js";
 import type * as functions from "../functions.js";
@@ -26,6 +21,11 @@ import type * as namespace from "../namespace.js";
 import type * as scrap from "../scrap.js";
 import type * as users from "../users.js";
 
+import type {
+  ApiFromModules,
+  FilterApi,
+  FunctionReference,
+} from "convex/server";
 /**
  * A utility for referencing Convex functions in your app's API.
  *
@@ -46,13 +46,406 @@ declare const fullApi: ApiFromModules<{
   scrap: typeof scrap;
   users: typeof users;
 }>;
+declare const fullApiWithMounts: typeof fullApi;
+
 export declare const api: FilterApi<
-  typeof fullApi,
+  typeof fullApiWithMounts,
   FunctionReference<any, "public">
 >;
 export declare const internal: FilterApi<
-  typeof fullApi,
+  typeof fullApiWithMounts,
   FunctionReference<any, "internal">
 >;
+
+export declare const components: {
+  leaderboard: {
+    btree: {
+      aggregateBetween: FunctionReference<
+        "query",
+        "internal",
+        { k1?: any; k2?: any },
+        { count: number; sum: number }
+      >;
+      atNegativeOffset: FunctionReference<
+        "query",
+        "internal",
+        { k1?: any; k2?: any; offset: number },
+        { k: any; s: number; v: any }
+      >;
+      atOffset: FunctionReference<
+        "query",
+        "internal",
+        { k1?: any; k2?: any; offset: number },
+        { k: any; s: number; v: any }
+      >;
+      count: FunctionReference<"query", "internal", {}, any>;
+      get: FunctionReference<
+        "query",
+        "internal",
+        { key: any },
+        null | { k: any; s: number; v: any }
+      >;
+      offset: FunctionReference<
+        "query",
+        "internal",
+        { k1?: any; key: any },
+        number
+      >;
+      offsetUntil: FunctionReference<
+        "query",
+        "internal",
+        { k2?: any; key: any },
+        number
+      >;
+      paginate: FunctionReference<
+        "query",
+        "internal",
+        {
+          cursor?: string;
+          k1?: any;
+          k2?: any;
+          limit: number;
+          order: "asc" | "desc";
+        },
+        {
+          cursor: string;
+          isDone: boolean;
+          page: Array<{ k: any; s: number; v: any }>;
+        }
+      >;
+      sum: FunctionReference<"query", "internal", {}, number>;
+      validate: FunctionReference<"query", "internal", {}, any>;
+    };
+    inspect: {
+      display: FunctionReference<"query", "internal", {}, any>;
+      dump: FunctionReference<"query", "internal", {}, string>;
+      inspectNode: FunctionReference<
+        "query",
+        "internal",
+        { node?: string },
+        null
+      >;
+    };
+    public: {
+      clear: FunctionReference<
+        "mutation",
+        "internal",
+        { maxNodeSize?: number; rootLazy?: boolean },
+        null
+      >;
+      deleteIfExists: FunctionReference<
+        "mutation",
+        "internal",
+        { key: any },
+        any
+      >;
+      delete_: FunctionReference<"mutation", "internal", { key: any }, null>;
+      init: FunctionReference<
+        "mutation",
+        "internal",
+        { maxNodeSize?: number; rootLazy?: boolean },
+        null
+      >;
+      insert: FunctionReference<
+        "mutation",
+        "internal",
+        { key: any; summand?: number; value: any },
+        null
+      >;
+      makeRootLazy: FunctionReference<"mutation", "internal", {}, null>;
+      replace: FunctionReference<
+        "mutation",
+        "internal",
+        { currentKey: any; newKey: any; summand?: number; value: any },
+        null
+      >;
+      replaceOrInsert: FunctionReference<
+        "mutation",
+        "internal",
+        { currentKey: any; newKey: any; summand?: number; value: any },
+        any
+      >;
+    };
+  };
+  crons: {
+    public: {
+      del: FunctionReference<
+        "mutation",
+        "internal",
+        { identifier: { id: string } | { name: string } },
+        null
+      >;
+      get: FunctionReference<
+        "query",
+        "internal",
+        { identifier: { id: string } | { name: string } },
+        {
+          args: Record<string, any>;
+          functionHandle: string;
+          id: string;
+          name?: string;
+          schedule:
+            | { kind: "interval"; ms: number }
+            | { cronspec: string; kind: "cron" };
+        } | null
+      >;
+      list: FunctionReference<
+        "query",
+        "internal",
+        {},
+        Array<{
+          args: Record<string, any>;
+          functionHandle: string;
+          id: string;
+          name?: string;
+          schedule:
+            | { kind: "interval"; ms: number }
+            | { cronspec: string; kind: "cron" };
+        }>
+      >;
+      register: FunctionReference<
+        "mutation",
+        "internal",
+        {
+          args: Record<string, any>;
+          functionHandle: string;
+          name?: string;
+          schedule:
+            | { kind: "interval"; ms: number }
+            | { cronspec: string; kind: "cron" };
+        },
+        string
+      >;
+    };
+  };
+  actionCache: {
+    cache: {
+      get: FunctionReference<
+        "mutation",
+        "internal",
+        { args: any; name: string; ttl: number | null },
+        any | null
+      >;
+      put: FunctionReference<
+        "mutation",
+        "internal",
+        { args: any; name: string; ttl: number | null; value: any },
+        null
+      >;
+    };
+    crons: {
+      purge: FunctionReference<
+        "mutation",
+        "internal",
+        { expiresAt?: number },
+        null
+      >;
+    };
+    lib: {
+      fetch: FunctionReference<
+        "action",
+        "internal",
+        { args: any; fn: string; name: string; ttl: number | null },
+        any
+      >;
+      remove: FunctionReference<
+        "mutation",
+        "internal",
+        { args: any; name: string },
+        null
+      >;
+      removeAll: FunctionReference<
+        "mutation",
+        "internal",
+        { before?: number; name?: string },
+        null
+      >;
+    };
+  };
+  ratelimiter: {
+    public: {
+      checkRateLimit: FunctionReference<
+        "query",
+        "internal",
+        {
+          config:
+            | {
+                capacity?: number;
+                kind: "token bucket";
+                maxReserved?: number;
+                period: number;
+                rate: number;
+                shards?: number;
+              }
+            | {
+                capacity?: number;
+                kind: "fixed window";
+                maxReserved?: number;
+                period: number;
+                rate: number;
+                shards?: number;
+                start?: number;
+              };
+          count?: number;
+          key?: string;
+          name: string;
+          reserve?: boolean;
+          throws?: boolean;
+        },
+        { ok: true; retryAfter?: number } | { ok: false; retryAfter: number }
+      >;
+      rateLimit: FunctionReference<
+        "mutation",
+        "internal",
+        {
+          config:
+            | {
+                capacity?: number;
+                kind: "token bucket";
+                maxReserved?: number;
+                period: number;
+                rate: number;
+                shards?: number;
+              }
+            | {
+                capacity?: number;
+                kind: "fixed window";
+                maxReserved?: number;
+                period: number;
+                rate: number;
+                shards?: number;
+                start?: number;
+              };
+          count?: number;
+          key?: string;
+          name: string;
+          reserve?: boolean;
+          throws?: boolean;
+        },
+        { ok: true; retryAfter?: number } | { ok: false; retryAfter: number }
+      >;
+      resetRateLimit: FunctionReference<
+        "mutation",
+        "internal",
+        { key?: string; name: string },
+        null
+      >;
+    };
+  };
+  shardedCounter: {
+    public: {
+      add: FunctionReference<
+        "mutation",
+        "internal",
+        { count: number; name: string; shards?: number },
+        null
+      >;
+      count: FunctionReference<"query", "internal", { name: string }, number>;
+      estimateCount: FunctionReference<
+        "query",
+        "internal",
+        { name: string; readFromShards?: number; shards?: number },
+        any
+      >;
+      rebalance: FunctionReference<
+        "mutation",
+        "internal",
+        { name: string; shards?: number },
+        any
+      >;
+    };
+  };
+  migrations: {
+    public: {
+      cancel: FunctionReference<
+        "mutation",
+        "internal",
+        { name: string },
+        {
+          batchSize?: number;
+          cursor?: string | null;
+          isDone: boolean;
+          latestStart?: number;
+          name: string;
+          next?: Array<string>;
+          processed: number;
+          workerStatus?:
+            | "pending"
+            | "inProgress"
+            | "success"
+            | "failed"
+            | "canceled";
+        }
+      >;
+      cancelAll: FunctionReference<
+        "mutation",
+        "internal",
+        { sinceTs?: number },
+        Array<{
+          batchSize?: number;
+          cursor?: string | null;
+          isDone: boolean;
+          latestStart?: number;
+          name: string;
+          next?: Array<string>;
+          processed: number;
+          workerStatus?:
+            | "pending"
+            | "inProgress"
+            | "success"
+            | "failed"
+            | "canceled";
+        }>
+      >;
+      getStatus: FunctionReference<
+        "query",
+        "internal",
+        { limit?: number; migrationNames?: Array<string> },
+        Array<{
+          batchSize?: number;
+          cursor?: string | null;
+          isDone: boolean;
+          latestStart?: number;
+          name: string;
+          next?: Array<string>;
+          processed: number;
+          workerStatus?:
+            | "pending"
+            | "inProgress"
+            | "success"
+            | "failed"
+            | "canceled";
+        }>
+      >;
+      runMigration: FunctionReference<
+        "mutation",
+        "internal",
+        {
+          batchSize?: number;
+          cursor?: string | null;
+          dryRun: boolean;
+          fnHandle: string;
+          name: string;
+          next?: Array<{ fnHandle: string; name: string }>;
+        },
+        {
+          batchSize?: number;
+          cursor?: string | null;
+          isDone: boolean;
+          latestStart?: number;
+          name: string;
+          next?: Array<string>;
+          processed: number;
+          workerStatus?:
+            | "pending"
+            | "inProgress"
+            | "success"
+            | "failed"
+            | "canceled";
+        }
+      >;
+    };
+  };
+};
 
 /* prettier-ignore-end */
