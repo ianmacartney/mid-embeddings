4bbe10657d5dd4d30f589ad4335f693e05bf0ef8
Ian Macartney <ianhmacartney@gmail.com>
2024-10-18 02:15:36 -0700

    use rate limiter

--- convex/namespace.ts
+++ convex/namespace.ts
@@ -28,14 +28,15 @@ import { FunctionArgs, paginationOptsValidator } from "convex/server";
 import { omit } from "convex-helpers";
 import { nullThrows } from "convex-helpers";
 import { getTextByTitle } from "./embed";
-import { defineRateLimits } from "convex-helpers/server/rateLimit";
+import { RateLimiter } from "@convex-dev/ratelimiter";
+import { components } from "./_generated/api";
 
 const SECOND = 1000;
 const MINUTE = 60 * SECOND;
 const HOUR = 60 * MINUTE;
 const DAY = 24 * HOUR;
 
-const { rateLimit } = defineRateLimits({
+const rate = new RateLimiter(components.ratelimiter, {
   createNamespace: { kind: "token bucket", period: 10 * SECOND, rate: 1 },
   addText: { kind: "token bucket", period: DAY, rate: 10_000 },
   basicSearch: { kind: "token bucket", period: SECOND, rate: 1, capacity: 5 },
@@ -67,8 +68,7 @@ export const upsertNamespace = userMutation({
       }
       return existing._id;
     }
-    await rateLimit(ctx, {
-      name: "createNamespace",
+    await rate.limit(ctx, "createNamespace", {
       key: ctx.user._id,
       throws: true,
     });
@@ -111,7 +111,7 @@ export const rateLimitAdds = internalMutation({
     count: v.number(),
   },
   handler: (ctx, { count, userId: key }) =>
-    rateLimit(ctx, { name: "addText", count, key, throws: true }),
+    rate.limit(ctx, "addText", { count, key, throws: true }),
 });
 
 export const addText = namespaceAdminAction({
@@ -184,7 +184,7 @@ export const listMidpoints = namespaceAdminQuery({
 export const rateLimitBasicSearch = internalMutation({
   args: { userId: v.id("users") },
   handler: (ctx, args) =>
-    rateLimit(ctx, { name: "basicSearch", key: args.userId, throws: true }),
+    rate.limit(ctx, "basicSearch", { key: args.userId, throws: true }),
 });
 
 export const basicVectorSearch = namespaceUserAction({
@@ -227,7 +227,7 @@ export const getResults = internalQuery({
 export const rateLimitMidSearch = internalMutation({
   args: { userId: v.id("users") },
   handler: (ctx, args) =>
-    rateLimit(ctx, { name: "midSearch", key: args.userId, throws: true }),
+    rate.limit(ctx, "midSearch", { key: args.userId, throws: true }),
 });
 
 function reciprocalRankFusion(aIndex: number, bIndex: number) {
